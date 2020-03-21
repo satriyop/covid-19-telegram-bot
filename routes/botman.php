4d22709bd56b3d0c062c128e2b7034d2ca328b7f
satriyop <satriyopamungkas@gmail.com>
2020-03-22 02:48:52 +0700

    Add Halo as greeting

--- routes/botman.php
+++ routes/botman.php
@@ -6,9 +6,13 @@ use App\Http\Controllers\CovidInfoController;
 $botman = resolve('botman');
 
 $botman->hears('Hi', function ($bot) {
-    $greeting = "Informasi tentang Covid-19 di Indonesia. Ketik info (untuk rangkuman informasi)";
+    $greeting = "Ketik : info (untuk mendapatkan rangkuman informasi covid-19 di Indonesia atau ketik : info nama_provinsi (untuk informasi rangkuman informasi covid-19 di provinsi tersebut.) ";
+    $bot->reply($greeting);
+});
+$botman->hears('Halo', function ($bot) {
+    $greeting = "Ketik : info (untuk mendapatkan rangkuman informasi covid-19 di Indonesia atau ketik : info nama_provinsi (untuk informasi rangkuman informasi covid-19 di provinsi tersebut.) ";
     $bot->reply($greeting);
 });
 // $botman->hears('Start conversation', BotManController::class.'@startConversation');
 
-// $botman->hears('info', CovidInfoController::class.'@getProvince');
+
